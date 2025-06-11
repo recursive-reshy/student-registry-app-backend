fbcdc081a4b23087a84788c60d800eab52d57124
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-11 18:31:26 +0800

    middleware\asyncWrapper.ts: Added helper function to handle async requests

---
+++ src/middleware/asyncWrapper.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express'
+
+const asyncWrapper = ( callback: Function ) => {
+  return async ( request: Request, response: Response, next: NextFunction ) => {
+    try {
+      await callback( request, response, next )
+    } catch ( error ) {
+      console.error( `Error in ${ callback.name }: ${ error }` )
+      next( error )
+    }
+  }
+}
