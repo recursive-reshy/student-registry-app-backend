8fa7c0394b69e1625aa39955fe0ad8090ba75c84
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-12 15:05:51 +0800

    package.json: Added helmet package for security headers

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 
 // Middlewares
 import cors from 'cors'
+import helmet from 'helmet'
 
 // Database
 import connect from './database/index.js'
@@ -12,7 +13,8 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 // Middlewares
-app.use( cors() )
+app.use( cors() ) // Cross-Origin Resource Sharing
+app.use( helmet() ) // Security headers
 app.use( express.json() )
 
 const start = async () => {
