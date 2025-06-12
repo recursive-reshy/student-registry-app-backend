64da239de642f2cea6c19b6882d46a1088459f1e
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-12 11:34:15 +0800

    src/server.ts: Connect to db at entry point

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express'
 
+// Middlewares
 import cors from 'cors'
+
+// Database
+import connect from './database/connection.js'
+
 
 // App config
 const PORT = process.env.PORT || 5000
@@ -13,11 +18,15 @@ app.use( express.json() )
 const start = async () => {
   try {
     console.log( 'Starting server...' )
+
+    // Connect to the database
+    await connect()
+
     app.listen( PORT, () => {
       console.log( `Server is running on port ${ PORT }` )
     } )
   } catch (error) {
-    console.error( `Error starting the server: ${ error }` )
+    console.error( `Error while starting the server: ${ error }` )
   }
 }
 
