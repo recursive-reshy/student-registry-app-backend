c569e4a79b13d5207f7344d8c9ed526c7d193c16
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-13 16:19:39 +0800

    controllers/teachers.ts: Added repo methods

--- src/controllers/teachers.ts
+++ src/controllers/teachers.ts
@@ -2,7 +2,9 @@ import { Request, Response } from 'express'
 
 import asyncWrapper from '../middleware/asyncWrapper.js'
 
-import { query } from '../database/index.js'
+// Repository methods
+import { save, findAll, findByEmail } from '../repositories/teachers.js'
+
 
 
 // Create
@@ -13,56 +15,31 @@ const createTeacher = asyncWrapper( async ( req: Request, res: Response ) => {
     return res.status( 400 ).json( { error: 'Name ad email is required' } )
   }
 
-  // TODO: Check if teacher already exists
-  const existingTeacher = await query( 'SELECT * FROM Teachers WHERE email = ?', [ email ] )
+  // Check if teacher already exists
+  const existingTeacher = await findByEmail( email )
   
   if( existingTeacher.results.length ) {
     return res.status( 400 ).json( { error: 'Teacher already exists' } )
   }
 
   // Insert teacher
-  const { results } = await query( 'INSERT INTO Teachers (name, email) VALUES (?, ?)', [ name, email ] )
+  const { results } = await save( { name, email } )
 
   console.log( results )
 
   return res.status( 201 ).json( { message: 'Teacher created successfully' } )
 } )
 
-
 // Get all teachers
 const getAllTeachers = asyncWrapper( async ( _: Request, res: Response ) => {
-  const { results } = await query( 'SELECT * FROM Teachers' )
+  const { results } = await findAll()
 
   return res.status( 200 ).json( results )
 } )
-
-// Currently only email is supported
-// TODO: Add support for other attributes
-const getTeacherByAttribute = asyncWrapper( async ( req: Request, res: Response ) => {
-  const { email } = req.query
-
-  if( !email ) {
-    return res.status( 400 ).json( { error: 'Email is required' } )
-  }
-
-  const { results } = await query( 'SELECT * FROM Teachers WHERE email = ?', [ email ] )
-
-  console.log( results, email )
-
-  if( !results.length ) {
-    return res.status( 404 ).json( { error: 'Teacher not found' } )
-  }
-
-  return res.status( 200 ).json( results[ 0 ] )
-} )
-
-
-// Update by email
 
 // Delete by email
 
 export { 
   createTeacher,
   getAllTeachers,
-  getTeacherByAttribute
 }
