3b70909d4ab8d7c09ab040a24c91bc136bafa4b0
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-14 16:30:32 +0800

    repositories\teacherStudentRegistration.ts: Added save function

--- src/repositories/teacherStudentRegistration.ts
+++ src/repositories/teacherStudentRegistration.ts
@@ -2,9 +2,36 @@ import { query, mutation, MutationResult, QueryResult } from '../database/index.
 
 import { TeacherStudentRegistration } from '../types/index.js'
 
-interface TeacherStudentRegistrationDto {
+interface CreateTeacherStudentRegistrationDto {
   teacherId: number
   studentId: number
 }
 
-export type { TeacherStudentRegistrationDto }
+const save = async ( { teacherId, studentId }: CreateTeacherStudentRegistrationDto ): Promise< MutationResult > => {
+  try {
+    const result = await mutation( 'INSERT INTO TeacherStudentRegistration ( teacherId, studentId ) VALUES ( ?, ? )', [ teacherId, studentId ] )
+
+    return result
+  } catch ( error ) {
+    console.error( `Error while creating teacher student registration in database: ${ error }` )
+    throw error
+  }
+}
+
+const findByTeacherIdAndStudentId = async ( teacherId: number, studentId: number ): Promise< QueryResult< TeacherStudentRegistration > > => {
+  try {
+    const result = await query( 'SELECT * FROM TeacherStudentRegistration WHERE teacherId = ? AND studentId = ?', [ teacherId, studentId ] )
+
+    return result
+  } catch ( error ) {
+    console.error( `Error while fetching teacher student registration by teacherId and studentId from database: ${ error }` )
+    throw error
+  }
+}
+
+export { 
+  save,
+  findByTeacherIdAndStudentId
+}
+
+export type { CreateTeacherStudentRegistrationDto } 
