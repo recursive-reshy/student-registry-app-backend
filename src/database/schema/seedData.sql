2d6f42fdfdb484beba35e22cd8e339de36ce1208
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-14 13:51:03 +0800

    database\schema\seedData.sql: Added script for seeding data

---
+++ src/database/schema/seedData.sql
@@ -0,0 +1,25 @@
+-- Teachers data
+INSERT INTO teachers (email, name) VALUES
+('rick.sanchez@interdimensional.edu', 'Rick Sanchez'),
+('morty.smith@school.edu', 'Morty Smith'),
+('summer.smith@school.edu', 'Summer Smith'),
+('jerry.smith@unemployed.com', 'Jerry Smith'),
+('beth.smith@hospital.edu', 'Beth Smith'),
+('birdperson@birdworld.edu', 'Birdperson'),
+('squanch.squanch@squanchnet.edu', 'Squanch'),
+('unity.collective@mindcontrol.edu', 'Unity'),
+('evil.morty@citadel.edu', 'Evil Morty'),
+('meeseeks@lookatus.edu', 'Mr. Meeseeks');
+
+-- Students data
+INSERT INTO students (email, name) VALUES
+('sterling.archer@isis.spy', 'Sterling Archer'),
+('lana.kane@isis.spy', 'Lana Kane'),
+('malory.archer@isis.spy', 'Malory Archer'),
+('cyril.figgis@isis.spy', 'Cyril Figgis'),
+('pam.poovey@isis.spy', 'Pam Poovey'),
+('cheryl.tunt@isis.spy', 'Cheryl Tunt'),
+('ray.gillette@isis.spy', 'Ray Gillette'),
+('krieger.algernop@isis.spy', 'Dr. Krieger'),
+('woodhouse.arthur@isis.spy', 'Woodhouse'),
+('barry.dylan@isis.spy', 'Barry Dylan');
