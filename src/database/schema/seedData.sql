1373ba7ce91aad8ca45a4a7ca0090bccbb1508f8
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-15 14:11:39 +0800

    database\schema\seedData.sql: Updated script to insert teach student registrations

--- src/database/schema/seedData.sql
+++ src/database/schema/seedData.sql
@@ -1,3 +1,5 @@
+-- Seed data for the database for testing
+
 -- Teachers data
 INSERT INTO teachers (email, name) VALUES
 ('rick.sanchez@interdimensional.edu', 'Rick Sanchez'),
@@ -23,3 +25,72 @@ INSERT INTO students (email, name) VALUES
 ('krieger.algernop@isis.spy', 'Dr. Krieger'),
 ('woodhouse.arthur@isis.spy', 'Woodhouse'),
 ('barry.dylan@isis.spy', 'Barry Dylan');
+
+-- Teacher-Student data
+INSERT INTO teacherstudentregistration (teacherId, studentId)
+SELECT t.id, s.id
+FROM teachers t, students s
+WHERE t.email = 'rick.sanchez@interdimensional.edu' 
+AND s.email IN (
+    'lana.kane@isis.spy',           
+    'krieger.algernop@isis.spy',  
+    'ray.gillette@isis.spy',      
+    'sterling.archer@isis.spy'    
+);
+
+INSERT INTO teacherstudentregistration (teacherId, studentId)
+SELECT t.id, s.id
+FROM teachers t, students s
+WHERE t.email = 'beth.smith@hospital.edu'
+AND s.email IN (
+    'krieger.algernop@isis.spy',
+    'lana.kane@isis.spy',       
+    'cyril.figgis@isis.spy'     
+);
+
+INSERT INTO teacherstudentregistration (teacherId, studentId)
+SELECT t.id, s.id
+FROM teachers t, students s
+WHERE t.email = 'summer.smith@school.edu'
+AND s.email IN (
+    'sterling.archer@isis.spy',
+    'cheryl.tunt@isis.spy',    
+    'pam.poovey@isis.spy'      
+);
+
+INSERT INTO teacherstudentregistration (teacherId, studentId)
+SELECT t.id, s.id
+FROM teachers t, students s
+WHERE t.email = 'morty.smith@school.edu'
+AND s.email IN (
+    'cyril.figgis@isis.spy',   
+    'woodhouse.arthur@isis.spy',
+    'barry.dylan@isis.spy'     
+);
+
+INSERT INTO teacherstudentregistration (teacherId, studentId)
+SELECT t.id, s.id
+FROM teachers t, students s
+WHERE t.email = 'evil.morty@citadel.edu'
+AND s.email IN (
+    'malory.archer@isis.spy',       
+    'sterling.archer@isis.spy',     
+    'barry.dylan@isis.spy'          
+);
+
+INSERT INTO teacherstudentregistration (teacherId, studentId)
+SELECT t.id, s.id
+FROM teachers t, students s
+WHERE t.email = 'jerry.smith@unemployed.com'
+AND s.email IN (
+    'cheryl.tunt@isis.spy'          
+);
+
+INSERT INTO teacherstudentregistration (teacherId, studentId)
+SELECT t.id, s.id
+FROM teachers t, students s
+WHERE t.email = 'unity.collective@mindcontrol.edu'
+AND s.email IN (
+    'pam.poovey@isis.spy',
+    'ray.gillette@isis.spy'
+);
