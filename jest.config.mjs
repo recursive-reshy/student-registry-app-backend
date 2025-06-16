a411e8df03d6cd1a6401df6c01555b62e1f12aca
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-16 21:18:11 +0800

    package.json: Adding mocking libraries

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,7 +2,24 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: [ './src/tests' ],
-  transform: { '^.+\\.tsx?$': 'ts-jest' },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: {
+          ignoreCodes: [1343]
+        },
+        astTransformers: {
+          before: [
+            {
+              path: 'node_modules/ts-jest-mock-import-meta',  // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
+              options: { metaObjectReplacement: { url: 'https://www.url.com' } }
+            }
+          ]
+        }
+      }
+    ]
+  },
   testRegex: '((\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleDirectories: [ 'node_modules', 'src' ],
